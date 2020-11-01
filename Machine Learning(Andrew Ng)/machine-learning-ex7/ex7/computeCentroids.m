64ca6a37b6622a52e05d756f61751f922c8162f9
Three <36061421+ThreeSR@users.noreply.github.com>
2020-11-01 16:23:43 +0800

    Add files via upload

---
+++ Machine Learning(Andrew Ng)/machine-learning-ex7/ex7/computeCentroids.m
@@ -0,0 +1,37 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+for k=1:K
+   centroids(k, :) = mean(X(idx==k, :)); % 就是计算RGB三通道的value的均值，最后output一个结果。不需要一个物理位置，可以是一个虚值，最后覆盖好自己group的pixel即可
+end
+
+% =============================================================
+
+
+end
+
